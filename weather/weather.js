ec2eb7eba4449c4457db64a64186e269ad5a97bd
Adekias <150199271+Adekias@users.noreply.github.com>
2026-04-21 14:39:13 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -1,13 +1,11 @@
-//create a new map centered on the continental US
+// Create a new map centered on the continental US
 var map = L.map('map').setView([38, -95], 4);
 
-//add OpenStreetMap to the map
-var OpenStreetMap_Mapnik = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
+// Add OpenStreetMap to the map
+var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var basemap = L.tileLayer(basemapUrl).addTo(map);
 
-//add weather radar to map
+// Add weather radar to the map
 var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
 var radarDisplayOptions = {
   layers: 'nexrad-n0r-900913',
@@ -16,19 +14,19 @@ var radarDisplayOptions = {
 };
 var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
 
-//Get GeoJSON data from the NWS weather alerts API
+// Get GeoJSON data from the NWS weather alerts API
 var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
 $.getJSON(weatherAlertsUrl, function(data) {
-L.geoJSON(data, {
-style: function(feature){
-  var alertColor = 'orange';
-  if (feature.properties.severity === 'Severe') alertColor = 'red';
-  return { color: alertColor };
-},
-
-//Add a popupon each feature showing the NWS alert headline
-onEachFeature: function(feature, layer) {
-  layer.bindPopup(feature.properties.headline);
-}
-}).addTo(map);
+  L.geoJSON(data, {
+    // Color all alert polygons orange, but color Severe polygons red
+    style: function(feature){
+      var alertColor = 'orange';
+      if (feature.properties.severity === 'Severe') alertColor = 'red';
+      return { color: alertColor }
+    },
+    // Add a popup on each feature showing the NWS alert headline
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup(feature.properties.headline);
+    }
+  }).addTo(map);
 });
